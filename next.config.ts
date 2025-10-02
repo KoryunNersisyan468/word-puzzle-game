8e159724f518e5e22a44c983e3176f668087fae6
koryunnersisyan468 <koryun468@gamil.com>
2025-10-02 22:08:38 +0400

    Commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 output: 'export',           
-  basePath: '/word-puzzle-game'
+  basePath: '/word-puzzle-game',
+  assetPrefix: '/word-puzzle-game/'
 };
 
 export default nextConfig;
