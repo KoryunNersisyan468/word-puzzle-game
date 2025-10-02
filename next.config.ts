8e6121ef21a67cbac5881d0cbee7431571746201
Koryun <koryun468@gmail.com>
2025-10-02 22:26:55 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 output: 'export',           
   basePath: '/word-puzzle-game',
-  assetPrefix: '/word-puzzle-game/'
+  assetPrefix: '/word-puzzle-game/',
+  trailingSlash: true,
+  unoptimized: true,
 };
 
 export default nextConfig;
